97f1bcec9ed55e2e01e1bbb6c53a360a57e51ecf
divyajai <48206558+divyajai@users.noreply.github.com>
2025-11-19 11:33:41 +0530

    Change virtual network name to 'policytest-vnet'
    Updated the virtual network name to 'policytest-vnet'.

--- network.tf
+++ network.tf
@@ -1,10 +1,9 @@
 # Virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
+  name                = policytest-vnet
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = var.vnet_address_space
-  tags                = var.tags
 }
 
 # Subnet
