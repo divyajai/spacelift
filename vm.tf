5a5ead7280f624b6f7ddb35a2979e5a88594c0a5
divyajai <48206558+divyajai@users.noreply.github.com>
2025-11-19 11:27:55 +0530

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,30 @@
+# Linux VM using managed disks
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = var.vm_name
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  computer_name         = var.vm_name
+  provision_vm_agent    = true
+  tags                  = var.tags
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "${var.vm_name}-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
